a640b8b8a52ec8be5467003f2da4c8296fec7adf
Ioannis Charamis <dev@charamis.org>
2025-02-08 23:15:23 +0200

    add createsuperuser command on dev image

--- scripts/entrypoints/dev-entrypoint.sh
+++ scripts/entrypoints/dev-entrypoint.sh
@@ -8,6 +8,7 @@ cd $APP_DIR
 
 ../apply_migrations.sh
 
-python manage.py createsuperuser # for dev env only
+# For dev env only, continue even if it fails - when superuser already exists
+python manage.py createsuperuser --username=admin --email=admin@admin.com --noinput || echo "Superuser already exists!"
 
 exec python manage.py runserver 0.0.0.0:3000
